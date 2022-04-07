daae31eecb48ee554555a849704040ab9be43743
Sudipta Banerjee <sudiptaaps@gmail.com>
2022-04-08 00:14:05 +0530

    some sop commit

--- src/main/java/programming/practice/SubClass.java
+++ src/main/java/programming/practice/SubClass.java
@@ -2,7 +2,7 @@ package programming.practice;
 
 public class SubClass extends BaseClass{
     public void someMethod(){
-
+        System.out.println("some method from Sub class");
     }
     public static void main(String[] as){
 //        BaseClass b = new BaseClass();
